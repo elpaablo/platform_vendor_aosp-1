syberia-project/platform_vendor_syberia

Update path for livedisplay permissions
Settings moved to product

Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: Ie35131721e115a4b0a64bf90e0950cb63f744cff

config/common.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/syberia/prebuilt/common/etc/privapp-permissions-syberia.xml:system/etc/permissions/privapp-permissions-syberia.xml \
     vendor/syberia/prebuilt/common/etc/privapp-permissions-googleapps.xml:system/etc/permissions/privapp-permissions-googleapps.xml \
-    vendor/syberia/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml \
+    vendor/syberia/config/permissions/privapp-permissions-custom.xml:system/product/etc/permissions/privapp-permissions-custom.xml \
     vendor/syberia/prebuilt/common/etc/privapp-permissions-turbo.xml:system/etc/permissions/privapp-permissions-turbo.xml
 
 

elpaablo/platform_vendor_aosp-1

config/common.mk
@@ -64,7 +64,7 @@ endif
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml \
-		vendor/arrow/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml \
+		vendor/arrow/config/permissions/privapp-permissions-custom.xml:system/product/etc/permissions/privapp-permissions-custom.xml \
 
 # Power whitelist
 PRODUCT_COPY_FILES += \
@@ -99,8 +99,7 @@ include vendor/arrow/config/sounds.mk
 # Adblock
 #PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/hosts.aicp_adblock:$(TARGET_COPY_OUT_SYSTEM)/etc/hosts.aicp_adblock
-    
+
 ifeq ($(WITH_GAPPS),true)
 include vendor/gapps/gapps.mk
 endif
-